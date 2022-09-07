6b819d8b3030abaf107b8d2f871854400b782dc7
zmaaf <zmaaf@connect.ust.hk>
2022-09-07 13:40:15 +0800

    added a spetograph

--- log_scale_helper.js
+++ log_scale_helper.js
@@ -15,11 +15,46 @@ spectro_vis.copy(spectro_vis, 0, 0, spectro_vis.width, spectro_vis.height, -spee
     }
     var c = value;
     spectro_vis.noStroke();
-    spectro_vis.colorMode(HSB);
+    spectro_vis.colorMode(spectro_vis.HSB);
     spectro_vis.fill(c, 255, c);
     var percent = i / input_spectrum.length;
     var y = percent * spectro_vis.height;
     spectro_vis.rect(spectro_vis.width - speed, spectro_vis.height - y, speed, spectro_vis.height / input_spectrum.length);
+  }
+
+  
+  
+}
+
+
+function process_spectrograph_fft(input_spectrum){
+  
+    // copy the sketch and move it over based on the speed
+//spectro_vis.background(30);
+spectro_vis.copy(spectro_vis, 0, 0, spectro_vis.width, spectro_vis.height, -speed, 0, spectro_vis.width, spectro_vis.height);
+
+  // iterate thru current freq spectrum
+  for (var i = 0; i < input_spectrum.length; i++) {
+    var value;
+    if (logView) {
+      logIndex = logScale(i, input_spectrum.length);
+      value = (1+input_spectrum[logIndex]/100)*255;
+    } else {
+      value = (1+input_spectrum[i]/100)*255;
+    }
+
+    if (i==0){
+          //console.log(value)
+}
+    var c = value;
+    spectro_vis.noStroke();
+    spectro_vis.colorMode(spectro_vis.HSB);
+    spectro_vis.fill(c, 255, c);
+    var percent = i / input_spectrum.length;
+    var y = percent * spectro_vis.height;
+    //console.log(spectro_vis.width - speed);
+    spectro_vis.rect(spectro_vis.width - speed, spectro_vis.height -y, speed, spectro_vis.height / input_spectrum.length);
+    //spectro_vis.background(c, 255, c)
   }
 
   
